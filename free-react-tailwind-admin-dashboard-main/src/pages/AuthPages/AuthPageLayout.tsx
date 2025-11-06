6a5d94ad5b130871200a3bce0bc88a94880314d8
tejas-tidke <tejastidke007@gmail.com>
2025-11-06 11:45:03 +0530

    Commit

--- free-react-tailwind-admin-dashboard-main/src/pages/AuthPages/AuthPageLayout.tsx
+++ free-react-tailwind-admin-dashboard-main/src/pages/AuthPages/AuthPageLayout.tsx
@@ -26,7 +26,7 @@ export default function AuthLayout({
                 />
               </Link>
               <p className="text-center text-gray-400 dark:text-white/60">
-                Free and Open-Source Tailwind CSS Admin Dashboard Template
+                Holograph - Your Next-Gen Enterprise Solutions Partner.
               </p>
             </div>
           </div>
